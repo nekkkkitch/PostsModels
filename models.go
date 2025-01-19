aaaab5e64f69e9f866202ca85624edce01525868
nekkkkitch <streamerpups@gmail.com>
2025-01-19 20:34:30 +0700

    addNotification

--- models.go
+++ models.go
@@ -6,10 +6,21 @@ import (
 	"github.com/google/uuid"
 )
 
+type User struct {
+	Login    string `json:"login"`
+	Password string `json:"password"`
+}
+
 type Post struct {
 	ID             uuid.UUID `json:"id"`
 	Title          string    `json:"title"`
 	Description    string    `json:"description"`
 	Owner          string    `json:"owner"`
 	DateOfCreation time.Time `json:"date_of_creation"`
+}
+
+type Notification struct {
+	Creator     string `json:"creator"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
 }
